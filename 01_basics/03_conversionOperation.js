89efd244130f23987f985782d1e1ca83b4bd1fe3
Arijit Debnath <75267807+iamarijitdebnath@users.noreply.github.com>
2023-09-25 14:14:17 +0000

    Add Operation topic

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,8 +15,19 @@ let valueInNumber = Number(score);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 =>true; 0 => false
 // "" => false
 // "Arijit" => true
+
+
+// ************* Operation *************
+
+let value =3
+let negValue =-value
+// console.log(negValue)
+
+let str1="Hello"
+let str2= " Arijit"
+console.log(str1+str2);
