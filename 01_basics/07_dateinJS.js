71f86a25e6e29fca1e480ee8eacdda13d2e2f349
Arijit Debnath <75267807+iamarijitdebnath@users.noreply.github.com>
2023-10-31 17:44:11 +0000

    Learn about date

---
+++ 01_basics/07_dateinJS.js
@@ -0,0 +1,20 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCratedDate = new Date(2023,0,23)
+// let myCratedDate = new Date(2023,0,23,5,3)
+// let myCratedDate = new Date("2023-01-08")
+let myCratedDate = new Date("01-08-2001")
+
+// console.log(myCratedDate.toDateString())
+// console.log(myCratedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
