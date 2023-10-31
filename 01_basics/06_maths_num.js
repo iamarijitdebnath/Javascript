5add665ead5aac92e1081e6375d341aa72cbee07
Arijit Debnath <75267807+iamarijitdebnath@users.noreply.github.com>
2023-10-31 16:05:29 +0000

    Maths and num

---
+++ 01_basics/06_maths_num.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.4543
+
+// console.log(otherNumber.toPrecision(3))
+
+
+const hunderds =100000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+
+// +++++++++ Maths ++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) 
+
