800831152afae3ad54dc81dde6300a5558077299
Arijit Debnath <75267807+iamarijitdebnath@users.noreply.github.com>
2023-11-05 07:01:37 +0000

    function basics

---
+++ 03_basics/01_function.js
@@ -0,0 +1,20 @@
+function sayMyName(){
+    console.log("A");
+    console.log("R");
+    console.log("I");
+    console.log("J");
+    console.log("I");
+    console.log("T");
+}
+
+// sayMyName();
+
+
+// function addTwoNumber(number1,number2){
+//         console.log(number1+number2);
+// }
+function addTwoNumber(number1,number2){
+        return number1+number2;
+}
+const result = addTwoNumber(1,2);
+console.log("Results : ",result)
